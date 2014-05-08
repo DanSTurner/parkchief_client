936b5fe159caf2792f322387b6d8fb3a8eea208a
Dan S Turner <contact@dansturner.com>
2014-05-08 12:22:50 -0700

    add client method parks

--- lib/parkchief_client/client.rb
+++ lib/parkchief_client/client.rb
@@ -3,12 +3,13 @@ module ParkchiefClient
   class Client
     include HTTParty
     base_uri 'http://0.0.0.0:3000/api/v1'
-    # def initialize
-    #   self.class.base_uri 'http://0.0.0.0:3000/api/v1'
-    # end
 
     def users
       self.class.get "/users"
+    end
+
+    def parks
+      self.class.get "/parks"
     end
 
   end
